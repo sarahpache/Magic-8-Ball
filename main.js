4bcbc63ffed7d13d75c3fa65e146597651b91654
sarahpache <104707621+sarahpache@users.noreply.github.com>
2022-05-23 16:42:31 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,37 +1,39 @@
 <script>
-    var userName = "";
-    let userQuestion = "";
-    let randomNumber = Math.floor(Math.random() * 8);
-    let eightBall = "";
+   const userName ='Sarah';
 
-
-if (userName != "") {
+if (userName) {
     console.log (`Hello, ${userName}!`);
 } 
 else {
     console.log (`Hello!`);
 }
-console.log (`${userName}, you asked: ${userQuestion}`);
-if (randomNumber === 1) {
+
+const userQuestion = "Will I learn to code?";
+
+console.log(`${userName} asked "${userQuestion}"`);
+
+const randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = '';
+
+if (randomNumber === 0) {
     eightBall = 'It is certain';
-} elseif (randomNumber = 2) {
+} else if (randomNumber === 1) {
     eightBall = 'It is decidedly so';
-} elseif (randomNumber === 3) {
+} else if (randomNumber === 2) {
     eightBall = 'Reply hazy try again';
-} elseif (randomNumber === 4) {
+} else if (randomNumber === 3) {
     eightBall = 'Cannot predict now';
-} elseif (randomNumber === 5) { 
- eightBall = 'Do not count on it'; 
-} elseif (randomNumber === 6) {
-     eightBall = 'My sources say no';
-} elseif (randomNumber === 7) { 
- eightBall = 'Outlook not so good';
-} elseif (randomNumber === 8) {
- eightBall = 'Signs point to yes';
-} elseif (randomNumber = 0) {
-eightBall = 'Try again!';
- } else  {
-    console.log ('Something is wrong');
+} else if (randomNumber === 4) {
+    eightBall = 'Do not count on it';
+} else if (randomNumber === 5) {
+    eightBall = 'My sources say no';
+} else if (randomNumber === 6) {
+    eightBall = 'Outlook not so good';
+} else if (randomNumber === 7) {
+    eightBall = 'Signs point to yes';
 }
-console.log (`eightball answer = ${eightBall}`);
-console.log (`Random number is ${randomNumber}`);
+
+console.log(`Your answer is: "${eightBall}"`);
+
+</script>
